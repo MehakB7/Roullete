e6a5ec32f707d49cda98e25e69a4aaee64efd81a
Mehakb77 <b7mehak@gmail.com>
2023-08-02 22:55:37 +0530

    updated the minRoullete

--- backend/contracts/MinRoullete.sol
+++ backend/contracts/MinRoullete.sol
@@ -10,15 +10,43 @@ contract MinRoullete {
         Black
     }
 
-    mapping(address => BetType) userToBet;
+    address owner;
+
+    uint expireTime = 1 minutes;
+
+    struct Game {
+        address winner;
+        mapping(address => bool) hasBet;
+        mapping(address => BetType) userToBet;
+        uint startTime;
+        bool isRunning;
+    }
+    uint gameId = 0;
+    mapping(uint => Game) games;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     receive() external payable {}
 
+    modifier onlyOwner() {
+        msg.sender == owner;
+        _;
+    }
+
     function placeBet(BetType bet) external payable {
         require(msg.value >= Bet, "Insuffiecient Balance");
-        userToBet[msg.sender] = bet;
-        // get random number and send it back to user
-
-        // send it back to user
+        require(!games[gameId].hasBet[msg.sender], "user Has already betted");
+        games[gameId].userToBet[msg.sender] = bet;
+        games[gameId].hasBet[msg.sender] = true;
     }
+
+    function startGame() external onlyOwner {
+        Game storage game = games[gameId];
+        game.startTime = block.timestamp;
+        game.isRunning = true;
+    }
+
+    function endGame() external onlyOwner {}
 }
